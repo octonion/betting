496166f7d6d1747ccefbf25d838910ca7c766a0d
Christopher D. Long <galizur@gmail.com>
2018-04-24 14:47:49 -0400

    Fractional expected log-growth.

--- kelly/fractional.py
+++ kelly/fractional.py
@@ -53,6 +53,15 @@ with open(sys.argv[1], 'r') as f:
             if (row['bet']):
                 race.at[i,'f'] = row['p']-row['p*']*(1-p_total)/(1-ip_total)
 
+        f = p_total-(1-p_total)*ip_total/(1-ip_total)
+        growth = 0.0
+        for i, row in race.iterrows():
+            if (row['bet']):
+                growth += row['p']*np.log(1-f+row['f']/row['p*'])
+                
+        growth += (1-p_total)*np.log(1-f)
+        #print("Expected log-growth =",growth)
+
         # Optimal bet fraction is as per binary Kelly
 
         optimal_f = p_total - (1-p_total)*ip_total/(1-ip_total)
@@ -69,25 +78,48 @@ with open(sys.argv[1], 'r') as f:
         print("Kullback-Leibler growth =",klg)
 
         print()
+        print("Full Kelly optimal bets:")
+        print()
         print(race)
         print()
 
         # Fractional Kelly
 
         print("Fraction of optimal Kelly =",fraction)
-        print("Fraction of bankroll =",fraction*optimal_f)
+        f = fraction*optimal_f
+        print("Fraction of bankroll =",f)
+        print()
+        
+        p_growth = 0.0
+        for i, row in race.iterrows():
+            if (row['bet']):
+                p_growth += row['p']*np.log(1-f+fraction*row['f']/row['p*'])
+
+        p_growth += (1-p_total)*np.log(1-f)
+
+        print("Proportional fractional expected log-growth =",p_growth)
 
         for i in reversed(race.index):
-            if (race.at[i,'bet']) and (fraction*optimal_f*(race.at[i,'p']*(1-ip_total)/p_total+race.at[i,'p*'])+(race.at[i,'p']*ip_total/p_total-race.at[i,'p*']) < 0):
+            if (race.at[i,'bet']) and (f*(race.at[i,'p']*(1-ip_total)/p_total+race.at[i,'p*'])+(race.at[i,'p']*ip_total/p_total-race.at[i,'p*']) < 0):
                 race.at[i,'bet'] = False
                 p_total = p_total-race.at[i,'p']
                 ip_total = ip_total-race.at[i,'p*']
 
+        growth = 0.0
         race['f'] = 0.0
         for i, row in race.iterrows():
             if (row['bet']):
-                race.at[i,'f'] = fraction*optimal_f*(row['p']*(1-ip_total)/p_total+row['p*'])+(row['p']*ip_total/p_total-row['p*'])
+                race.at[i,'f'] = f*(row['p']*(1-ip_total)/p_total+row['p*'])+(row['p']*ip_total/p_total-row['p*'])
 
+        for i, row in race.iterrows():
+            if (row['bet']):
+                growth += row['p']*np.log(1-f+row['f']/row['p*'])
+
+        growth += (1-p_total)*np.log(1-f)
+        print("Optimal fractional expected log-growth =",growth)
+
+        print()
+        print("Fractional Kelly optimal bets:")
         print()
         print(race)
         print()
